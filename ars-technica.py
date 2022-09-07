6d8e797ac73bf114c83e840b18ccb842854bbe20
Jonbiveo <logantedmonds@gmail.com>
2022-09-07 09:23:48 -0500

    Simple web scraping of ars technica set up for first 8 articles with title, background image, and description

--- ars-technica.py
+++ ars-technica.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import re
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -14,6 +16,21 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get('https://arstechnica.com/')
 
-for post in driver.find_elements(By.CLASS_NAME, 'tease.article'):
-    title = post.find_element(By.CSS_SELECTOR, '.article header > h2 > a').text
-    print(title)
+postings = []
+# .article header > h2 > a
+for post in driver.find_elements(By.XPATH, '//*[@id="main"]/section[1]/ul/li'):
+    title = post.find_element(By.XPATH, './/header/h2/a').text
+    img = post.find_element(By.XPATH, './/figure/div').value_of_css_property('background-image')
+    img_url = re.split('[""]',img)[1]
+    description = post.find_element(By.XPATH, './/header/p').text
+
+    post_item = {
+        'title': title,
+        'img': img_url,
+        'description': description
+    }
+
+    postings.append(post_item)
+
+df = pd.DataFrame(postings)
+print(df)
